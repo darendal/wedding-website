a7ff9d78c9ea9b81e1908267ae27f0bc7a702b5d
Brendan Ware <bware@mobileiron.com>
2018-08-27 14:14:47 -0700

    first pass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
-
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { RsvpComponent } from './rsvp/rsvp.component';
+import { PhotosComponent } from './photos/photos.component';
+import { EventsComponent } from './events/events.component';
+import { TravelComponent } from './travel/travel.component';
+import { RegistryComponent } from './registry/registry.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RsvpComponent,
+    PhotosComponent,
+    EventsComponent,
+    TravelComponent,
+    RegistryComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
