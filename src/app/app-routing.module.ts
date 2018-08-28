aa86c2efc7f9790e6c7f75c37f8254f90bc45acf
Brendan Ware <bware@mobileiron.com>
2018-08-27 23:05:06 -0700

    Add missing files to project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {RsvpComponent} from './rsvp/rsvp.component';
+import {PhotosComponent} from './photos/photos.component';
+import {EventsComponent} from './events/events.component';
+import {TravelComponent} from './travel/travel.component';
+import {RegistryComponent} from './registry/registry.component';
+
+const routes = [
+  {path: 'home',     component: HomeComponent},
+  {path: 'rsvp',     component: RsvpComponent},
+  {path: 'photos',   component: PhotosComponent},
+  {path: 'events',   component: EventsComponent},
+  {path: 'travel',   component: TravelComponent},
+  {path: 'registry', component: RegistryComponent}
+  ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule { }
