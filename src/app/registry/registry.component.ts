4ea1c4785375d27ea5da7028980c0df918794c7c
Brendan Ware <bware@mobileiron.com>
2019-04-28 18:13:03 -0700

    Add Frontgate registry

--- src/app/registry/registry.component.ts
+++ src/app/registry/registry.component.ts
@@ -9,6 +9,7 @@ export class RegistryComponent implements OnInit {
 
 
   amazonRegistry = 'https://www.amazon.com/wedding/brendan-ware-emily-buchroeder-cleveland-september-2019/registry/1UZKIENGYWMGI';
+  frontgateRegistry = 'https://www.frontgate.com/gr/8162535';
   constructor() { }
 
   ngOnInit() {
