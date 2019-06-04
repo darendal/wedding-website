0070537000b1128c4088bae222eec1812e8480c2
Brendan Ware <bware@mobileiron.com>
2019-06-04 19:10:55 -0400

    Add new registry

--- src/app/registry/registry.component.ts
+++ src/app/registry/registry.component.ts
@@ -11,6 +11,7 @@ export class RegistryComponent implements OnInit {
   amazonRegistry = 'https://www.amazon.com/wedding/brendan-ware-emily-buchroeder-cleveland-september-2019/registry/1UZKIENGYWMGI';
   frontgateRegistry = 'https://www.frontgate.com/gr/8162535';
   dillardsRegistry = 'https://www.dillards.com/registry/BRENDANWARE/132323940';
+  surLaTable = 'https://www.surlatable.com/registry/giftRegistryList.jsp?id=2002485685585';
   constructor() { }
 
   ngOnInit() {
